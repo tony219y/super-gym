0760ed3bd76626863c88517de9f4ea733f72a274
tony219y <aekanan2004@gmail.com>
2025-06-15 16:38:09 +0700

    Enhance API call handling and UI feedback in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,27 @@
 import { getHelloAPI } from "./services/home.services"
+import { useState } from "react";
 
 function App() {
+  const data = "API not called yet";
+  const [text, setText] = useState(data);
   const handleClick = async () => {
     const  data  = await getHelloAPI();
-    console.log(data);
+    if (!data) {
+      console.error("Failed to fetch data from API");
+      return;
+    }
+    setText(data.message);
   }
+
+
   return (
     <>
       <div className='flex flex-col w-full h-screen items-center justify-center bg-[#2f2f2f]'>
-        <h1 className="text-9xl font-bold text-white">Hello World!</h1>
-        <button onClick={handleClick} className=" p-4 border bg-white rounded-2xl shadow-md shadow-black/50 hover:bg-white/50 duration-300">CALL AN API</button>
+        <h1 className="text-9xl font-bold text-white">{text}</h1>
+        <div className="flex gap-4 mt-10">
+          <button onClick={handleClick} className=" py-2 px-4 border bg-white rounded-2xl shadow-md shadow-black/50 hover:bg-white/50 duration-300">CALL AN API</button>
+          <button onClick={()=> setText(data)} className=" py-2 px-4 border bg-white rounded-2xl shadow-md shadow-black/50 hover:bg-white/50 duration-300">Clear</button>
+        </div>
       </div>
     </>
   )
