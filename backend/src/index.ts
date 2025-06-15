357e5fac63c5ea6de4e889c243e4e156b315c91e
tony219y <aekanan2004@gmail.com>
2025-06-15 13:25:18 +0700

    update backend to change default port from 3000 to 4001

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4001;
 
 app.use(cors());
 app.use(express.json());
